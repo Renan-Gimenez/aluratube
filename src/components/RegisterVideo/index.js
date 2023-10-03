66de00fbdcea7b24589d5d7ca34bce395494cca2
Renan Gimenez <renangiimenez@gmail.com>
2023-10-02 23:53:06 -0300

    Insert video on database

--- src/components/RegisterVideo/index.js
+++ src/components/RegisterVideo/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { createClient } from "@supabase/supabase-js";
 import { StyledRegisterVideo } from "./styles";
 
 function useForm(propsDoForm) {
@@ -20,6 +21,14 @@ function useForm(propsDoForm) {
   };
 }
 
+const PROJECT_URL = "https://uidvmjolmjaclnengenj.supabase.co";
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVpZHZtam9sbWphY2xuZW5nZW5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYxMTM2NTUsImV4cCI6MjAxMTY4OTY1NX0.LKR5jvF38K0CTmS6sPepz_kin9MxE9HW_w-4Nlt4DNQ";
+const supabase = createClient(PROJECT_URL, API_KEY);
+
+function getThumbnail(url) {
+  return `https://img.youtube.com/vi/${url.split("v=")[1]}/hqdefault.jpg`;
+}
+
 export default function RegisterVideo() {
   const formCadastro = useForm({
     initialValues: { titulo: "Davy Jones", url: "https://youtube.com" },
@@ -35,12 +44,26 @@ export default function RegisterVideo() {
         <form
           onSubmit={(e) => {
             e.preventDefault();
+
+            supabase.from("video").insert({
+              title: formCadastro.values.titulo,
+              url: formCadastro.values.url,
+              thumb: getThumbnail(formCadastro.values.url),
+              playlist: "jogos"
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+            
             setFormVisivel(false);
             formCadastro.clearForm();
           }}
         >
           <div>
-            <button
+            <button type="button"
               className="close-modal"
               onClick={() => setFormVisivel(false)}
             >
